8a60f005190eca21e4960ada30def6bc7218bad3
thanhtayaung <thanhtayaung@ucstaungoo.edu.mm>
2024-04-20 00:05:06 +0700

    add post components

---
+++ first-app/src/components/Post.jsx
@@ -0,0 +1,14 @@
+const names = ["Maximiliam", "Manuel"];
+
+function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+  return (
+    <div>
+      <p suppressHydrationWarning>{chosenName}</p>
+      <p>React js is awesome!</p>
+    </div>
+  );
+}
+
+export default Post;
